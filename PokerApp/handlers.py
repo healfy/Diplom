95e24e161da24ca9d6805af8fc3f6d665abc1618
healfy <healfy92@gmail.com>
2018-07-31 14:12:20 +0300

    added handlers for hand_power, position on table, actions pre flop

--- PokerApp/handlers.py
+++ PokerApp/handlers.py
@@ -5,12 +5,40 @@ def preflop_actions(list_of_actions):
 
 
 def hand_power(card1, card2, position):
-    rank_ep = ['A', 'K', 'Q', 'J', 'T']
+    premium_rank = ['A', 'K', 'Q', 'J', 'T']
+    middle_rank = ['9', '8', '7']
     hand = card1 + card2
     if position == 'EP':
         if hand[1] == hand[3]:
             return True
-        elif hand[1] in rank_ep and hand[3] in rank_ep:
+        elif hand[1] in premium_rank and hand[3] in premium_rank:
             return True
-
+    elif position == 'MP':
+        if hand[1] == hand[3]:
+            return True
+        elif hand[1] in premium_rank and hand[3] in premium_rank:
+            return True
+        elif hand[0] == hand[2] and hand[1] in middle_rank and hand[3] \
+                in middle_rank:
+            return True
+    elif position == 'CO':
+        if hand[1] == hand[3]:
+            return True
+        elif hand[1] in premium_rank and hand[3] in premium_rank:
+            return True
+        elif hand[0] == hand[2] and hand[1] in middle_rank and hand[3] \
+                in middle_rank:
+            return True
+    elif position == 'BU':
+        if hand[1] == hand[3]:
+            return True
+        elif hand[1] in premium_rank and hand[3] in premium_rank:
+            return True
+        elif hand[1] in premium_rank and hand[3] in middle_rank or hand[1] \
+                in middle_rank and hand[3] in premium_rank:
+            return True
+    elif position == 'SB':
+        return True
+    elif position == 'BB':
+        return True
     return False
